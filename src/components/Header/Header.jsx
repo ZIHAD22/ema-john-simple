f856c1de26bc9353ad55c922701dd18aedfbfa99
ZIHAD22 <mdzihad2244@gmail.com>
2022-03-23 00:48:50 +0600

    header section responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,13 +5,38 @@ import "./Header.css";
 
 const Header = () => {
   return (
-    <nav className="header">
-      <img src={logo} alt="Logo" />
-      <div className="header-list" >
-        <a href="/shop">Shop</a>
-        <a href="/orders">Order</a>
-        <a href="/inventory">Inventory</a>
-        <a href="/about">About</a>
+    <nav className="navbar navbar-expand-lg navbar-dark header">
+      <div className="container">
+        <a className="navbar-brand" href="/">
+          <img src={logo} alt="" />
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav ms-auto">
+            <a className="nav-link active" aria-current="page" href="/shop">
+              Shop
+            </a>
+            <a className="nav-link" href="/Oder">
+              Order
+            </a>
+            <a className="nav-link" href="/Inventory">
+              Inventory
+            </a>
+            <a className="nav-link" href="/About">
+              About
+            </a>
+          </div>
+        </div>
       </div>
     </nav>
   );
